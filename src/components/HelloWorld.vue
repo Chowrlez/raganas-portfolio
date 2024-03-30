b304243a1339c21cd62a8afaca5ff98c1079a29b
Pareng Onin <turajosejose@gmail.com>
2024-03-30 20:52:40 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,236 @@
+  <template>
+    <div class="bg-dark">
+      <nav class="navbar navbar-expand-lg bg-body-tertiarysticky-top">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="center collapse navbar-collapse bg-dark mt-5" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link fs-5" aria-current="page" href="#home">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fs-5" href="#skills">Skills</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fs-5" href="#projects">Projects</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fs-5" href="#contact">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fs-5" href="#about">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div id="home" class="container">
+        <div class="row justify-content-center mt-5">
+          <div>
+            <h1 class="stylish-font1">
+            My Portfolio
+            </h1>
+          </div>
+            <div class="col-md-4">
+              <img class="img-fluid rounded mb-5" alt="Portrait" src="/header-image.jpg">
+            </div>
+        </div>
+        <div class="col-md-12 " id="description2">
+          <h1><a href="" class="text-decoration-none text-white">Cordova Public College</a></h1>
+
+        </div>
+
+        <h1 class="pt-4 mb-4 text-center text-white fw-bold stylish-font1">About Me</h1>
+
+        <div class="col-md-12" id="description2">
+              <h5 class="text-white">
+                Hello, everyone! My name is Charles Harley Raganas, and I'm currently pursuing a Bachelor of Science in Information Technology at Cordova Public College. I'm passionate about exploring the dynamic world of technology and its applications in solving real-world problems.
+
+                Studying Information Technology has been an exciting journey for me, as it allows me to delve into various aspects of computing, from software development to network administration. I'm enthusiastic about learning new programming languages, exploring different software tools, and understanding the latest trends in the IT industry.
+
+                Cordova Public College has provided me with a nurturing environment where I can grow both academically and personally. The faculty members are highly knowledgeable and supportive, always encouraging me to push my boundaries and strive for excellence.
+
+                Outside of academics, I enjoy engaging in extracurricular activities that complement my studies. Whether it's participating in hackathons, attending tech conferences, or collaborating on IT projects with fellow students, I'm always eager to expand my skills and broaden my horizons.
+
+                In the future, I aspire to leverage my education and experiences to make meaningful contributions to the field of Information Technology. I'm excited about the endless possibilities that lie ahead and look forward to embracing the challenges and opportunities that come my way.
+
+                Thank you for taking the time to get to know me. I'm thrilled to be part of this incredible journey, and I can't wait to see where it takes me!
+
+
+              </h5>
+          </div>
+      </div>
+
+        <div id="skills"></div>
+
+        <h2 class="pt-4 mb-4 text-center text-white fw-bold stylish-font3">Skills</h2>
+
+        <div class="container">
+            <div class="row justify-content-evenly px-lg-0 px-md-3 px-5">
+                <div class="col-lg-3 col-md-4 text-center py-4 my-3">
+                    <img src="bootstrap.png" width="80px" title="Bootstrap" class="zoom-in-effect">
+                </div>
+                <div class="col-lg-3 col-md-4 text-center py-4 my-3">
+                    <img src="css.png" width="80px" title="CSS" class="zoom-in-effect">
+                </div>
+                <div class="col-lg-3 col-md-4 text-center py-4 my-3">
+                    <img src="js.png" width="80px" title="JavaScript" class="zoom-in-effect">
+                </div>
+                <div class="col-lg-3 col-md-4 text-center py-4 my-3">
+                    <img src="php.png" width="140px" title="PHP" class="zoom-in-effect">
+                </div>
+                <div class="col-lg-3 col-md-4 text-center py-4 my-3">
+                    <img src="sql.png" width="130px" title="SQL" class="zoom-in-effect">
+                </div>
+                <div class="col-lg-3 col-md-4 text-center py-4 my-3">
+                    <img src="html.png" width="80px" title="HTML" class="zoom-in-effect">
+                </div>
+                <div class="col-lg-3 col-md-4 text-center py-4 my-3">
+                    <img src="vue.png" width="100px" title="Vue.js" class="zoom-in-effect">
+                </div>
+            </div>
+        </div>
+
+        <div id="projects"></div>
+
+      
+
+        <div id="contact"></div>
+        
+        <h1 class="pt-4 mb-4 text-center text-white fw-bold stylish-font1">Social Media</h1>
+
+        <div class="container">
+            <div class="row px-lg-12 px-md-12 mx-auto">
+                <div class="col-lg-4 col-md-4 text-center py-4 my-3">
+                <a href="https://github.com/Chowrlez">
+                  <img src="github.png" width="80px" title="Github" class="zoom-in-effect">
+                </a>
+                </div>
+                <div class="col-lg-4 col-md-4 text-center py-4 my-3">
+                  <a href="https://www.facebook.com/charlesharlyraganas?mibextid=JRoKGi">
+                    <img src="fb.png" width="80px" title="Facebook" class="zoom-in-effect">
+                  </a>  
+                </div>
+                <div class="col-lg-4 col-md-4 text-center py-4 my-3">
+                  <a href="https://www.instagram.com/charlesharly1/">
+                    <img src="ig.png" width="80px" title="Instagram" class="zoom-in-effect">
+                  </a>  
+                </div>
+                <div class="col-lg-4 col-md-4 text-center py-4 my-3">
+                  <a href="mailto:lancejavate2002@gmail.com" class="text-dark text-decoration-none">
+                    <h5 class="zoom-in-effect fw-bold"><i class="fas fa-envelope"></i> Email: lancejavate2002@gmail.com</h5>
+                  </a>  
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container-fluid bg-dark mt-5 footer">
+            <div class="row">
+                <div class="col-lg-4 p-4">
+                    <h5 class="mb-3 text-white">Links</h5>
+                    <a href="#home" class="d-inline-block mb-2 text-white text-decoration-none">Home</a> |
+                    <a href="#skills" class="d-inline-block mb-2 text-white text-decoration-none">Skills</a> |
+                    <a href="#projects" class="d-inline-block mb-2 text-white text-decoration-none">Projects</a> |
+                    <a href="#contact" class="d-inline-block mb-2 text-white text-decoration-none">Contact</a> |
+                    <a href="#about" class="d-inline-block mb-2 text-white text-decoration-none">About</a> 
+                </div>
+                <div class="col-lg-4 p-4">
+                    <h5 class="mb-3 text-white">Contact me</h5>
+                    <a href="" class="d-inline-block text-white text-decoration-none mx-2">
+                        <i class="fab fa-twitter"></i> Twitter
+                    </a> 
+                    <a href="" class="d-inline-block text-white text-decoration-none mx-2">
+                        <i class="fab fa-facebook me-1"></i> Facebook
+                    </a>
+                    <a href="" class="d-inline-block text-white text-decoration-none mx-2">
+                        <i class="fab fa-instagram me-1"></i> Instagram
+                    </a><br>
+                </div>
+                <div class="col-lg-4 p-4 my-auto">
+                    <h5 class="mb-3 text-white">All right reserve.</h5>
+                </div>
+            </div>
+        </div>
+      </div>
+  </template>
+
+  <script>
+
+  </script>
+
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  #description1 {
+    margin-top: 280px;
+  }
+
+  #description2 {
+    margin-top: 200px;
+  }
+
+  h1 {
+    font-size: 150px;
+  }
+
+  @media (max-width: 767px) {
+    #description1,
+    #description2 {
+      margin-top: 0; 
+    }
+    h1 {
+    font-size: 50px;
+  }
+  }
+
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  .stylish-font1 {
+    font-family: "Play", sans-serif;
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  .stylish-font2 {
+    font-family: "Dancing Script", cursive;
+    font-optical-sizing: auto;
+    font-weight: 700;
+    font-style: normal;
+    font-size: 27px; 
+  }
+  .stylish-font3 {
+    font-family: "Bebas Neue", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 60px; 
+  }
+  .stylish-font4 {
+    font-family: "Playfair Display", serif;
+    font-optical-sizing: auto;
+    font-weight: 700;
+    font-style: normal;
+    font-size: 22px; 
+  }
+
+  .center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  </style>
